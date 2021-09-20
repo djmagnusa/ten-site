26f75c8c9f1d3360b183dbdd2da16eb882e4b1c8
djmagnusa <pratush.bh@gmail.com>
2021-09-20 11:10:50 +0530

    Add benefits section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import "./App.css";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Benefits from './components/Benefits';
 
 const App = () => {
   return (
     <div>
         <Navbar />
         <Hero />
+        <Benefits />
     </div>
   )
 }

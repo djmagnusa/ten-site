3ab153334b52254ec1b9bcb4b5832af39f88025d
djmagnusa <pratush.bh@gmail.com>
2021-09-22 21:24:28 +0530

    Style contact section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
 import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Testimonials />
+        <Contact />
 
     </div>
   )

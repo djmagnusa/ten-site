c3833922ecba6913079d401ff47641179bffd241
djmagnusa <pratush.bh@gmail.com>
2021-09-22 21:22:41 +0530

    Add contact section

---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+    <div className="container-fluid contact-container">
+        <div className="wrapper">
+            <h2>CONTACT US</h2>
+            <form>
+            <div className="form-group">
+                <label for="name">Full Name</label>
+                <input type="text" name="Name" id="name" placeholder="Name" required minlength="3" maxlength="25" />
+            </div>
+            <div className="form-group">
+                <label for="email">Email Address</label>
+                <input type="email" name="Email" id="email" placeholder="email@domain.tld" required />
+            </div>
+            <div className="form-group">
+                <label for="message">Message</label>
+                <textarea name="Message" id="message" rows="5" placeholder="Type your message here...."></textarea>
+            </div>
+            <div className="form-group">
+                <button type="submit" className="submit"><i className="far fa-paper-plane"></i>Send</button>
+            </div>
+            </form>
+        </div>
+        
+     </div>
+    )
+}
+
+export default Contact
